8d3c262040bedc5702b85ba553a10185a729181b
Giuseppe Vietri <vietr002@umn.edu>
2023-01-20 08:22:24 -0600

    acs real

--- examples/acs/acs_real.py
+++ examples/acs/acs_real.py
@@ -12,6 +12,7 @@ from toy_datasets.sparse import get_sparse_dataset
 import matplotlib.pyplot as plt
 import time
 from utils.plot_low_dim_data import plot_2d_data
+from utils.utils_data import get_data
 
 
 def run_toy_example(algo,
@@ -31,16 +32,7 @@ def run_toy_example(algo,
     print(f'Saving in {folder}')
     os.makedirs(folder, exist_ok=True)
     def debug_fn(t, tempdata):
-        X = tempdata.to_numpy()
-        train_error = jnp.abs(stats_module.get_true_stats() - stats_module.get_stats_jax_jit(X)).max()
-        eval_error = jnp.abs(evaluate_stat_module.get_true_stats() - evaluate_stat_module.get_stats_jax_jit(X)).max()
-        if t == 0:
-            save_path = f'{folder}/{data_name}_original.png'
-        else:
-            save_path = f'{folder}/img_{t:04}.png'
-
-        plot_2d_data(tempdata.to_numpy(), title=f'epoch={t}:\nTrain error = {train_error:.3f}, Eval error={eval_error:.3f}', alpha=0.9, save_path=save_path)
-
+        pass
 
 
 
@@ -67,45 +59,41 @@ def run_toy_example(algo,
 
 
 
-DATASETS = {
-    # 'circles': get_circles_dataset(DATA_SIZE=10000),
-            # 'moons': get_moons_dataset(DATA_SIZE=10000),
-    'sparse': get_sparse_dataset(DATA_SIZE=10000),
-    # 'ACSreal' :
-}
 
 if __name__ == "__main__":
+    state = 'CA'
+    data_name = f'folktables_2018_real_{state}'
+    data = get_data(f'{data_name}-mixed-train',
+                    domain_name=f'domain/{data_name}-mixed',  root_path='../../data_files/folktables_datasets_real')
 
 
-    for data_name in DATASETS:
-        data = DATASETS[data_name]
 
 
-        # algo = PrivGAfast(
-        #     num_generations=100000,
-        #     strategy=SimpleGAforSyncDataFast(
-        #         domain=data.domain,
-        #         data_size=500,
-        #         population_size=100,
-        #         elite_size=5,
-        #         muta_rate=1,
-        #         mate_rate=1,
-        #     ),
-        #     print_progress=False)
-
-        algo = RelaxedProjectionPP(
+    algo = PrivGAfast(
+        num_generations=100000,
+        strategy=SimpleGAforSyncDataFast(
             domain=data.domain,
-            data_size=500,
-            learning_rate=(0.0001,),
-            print_progress=True)
+            data_size=2000,
+            population_size=100,
+            elite_size=5,
+            muta_rate=1,
+            mate_rate=1,
+        ),
+        print_progress=True)
 
-        hs_stats_module, _ = Halfspace4.get_kway_random_halfspaces(data.domain, k=1, rng=jax.random.PRNGKey(0),
-                                                                   random_hs=500)
-        ranges_stat_module, _ = Marginals.get_all_kway_mixed_combinations(data.domain, k_disc=1, k_real=2,
-                                                                          bins=[2, 4, 8, 16, 32, 64])
-        run_toy_example(algo, data,
-                        stats_module=hs_stats_module,
-                        evaluate_stat_module=ranges_stat_module,
-                        epsilon=1,
-                        rounds=40,
-                        adaptive=True)
+    # algo = RelaxedProjectionPP(
+    #     domain=data.domain,
+    #     data_size=500,
+    #     learning_rate=(0.0001,),
+    #     print_progress=True)
+
+    hs_stats_module, _ = Halfspace4.get_kway_random_halfspaces(data.domain, k=1, rng=jax.random.PRNGKey(0),
+                                                               random_hs=20000)
+    ranges_stat_module, _ = Marginals.get_all_kway_mixed_combinations(data.domain, k_disc=1, k_real=2,
+                                                                      bins=[2, 4, 8, 16, 32, 64])
+    run_toy_example(algo, data,
+                    stats_module=hs_stats_module,
+                    evaluate_stat_module=ranges_stat_module,
+                    epsilon=1,
+                    rounds=50,
+                    adaptive=True)
