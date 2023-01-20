544afeed2f2b77bade2c7783ce5ffea25ef16dba
Jingwu Tang <tangjingwu@pku.edu.cn>
2023-01-20 19:03:42 +0800

    update

--- stats/__init__.py
+++ stats/__init__.py
@@ -2,4 +2,5 @@ from stats.marginals import Marginals
 from stats.adaptive_statistic import AdaptiveStatisticState
 
 from stats.halfspaces_v2 import Halfspace
+from stats.halfspaces_v3 import Halfspace3
 from stats.marginals_prefix import MarginalsPrefix
