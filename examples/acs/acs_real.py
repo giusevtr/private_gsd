b75fe49b0d8368dc1b480ffaaddc4d9cbbe6241c
Giuseppe Vietri <vietr002@umn.edu>
2023-01-20 23:26:03 -0600

    ACSreal example with PrivGA and RAP++

--- examples/acs/acs_real.py
+++ examples/acs/acs_real.py
@@ -15,7 +15,7 @@ from utils.plot_low_dim_data import plot_2d_data
 from utils.utils_data import get_data
 
 
-def run_toy_example(algo,
+def run_acs_example(algo,
                     data,
                     stats_module,
                     evaluate_stat_module,
@@ -23,18 +23,15 @@ def run_toy_example(algo,
                     seed=0,
                     rounds=30,
                     num_sample=10,
-                    adaptive=False):
+                    adaptive=False,
+                    save_path='sync'):
 
     stats_module.fit(data)
     evaluate_stat_module.fit(data)
-
-    folder = f'images/{str(algo)}/{stats_module}/{data_name}/'
+    folder = f'images/{str(algo)}/{stats_module}/{data_name}/{epsilon:.2f}/{rounds}'
     print(f'Saving in {folder}')
     os.makedirs(folder, exist_ok=True)
-    def debug_fn(t, tempdata):
-        path = f'sync/sync_data_{t}.csv'
-        print(f'Saving {path}')
-        tempdata.df.to_csv(path, index=False)
+    path = f'{folder}/sync_data.csv'
 
 
 
@@ -45,10 +42,11 @@ def run_toy_example(algo,
 
     if adaptive:
         sync_data = algo.fit_dp_adaptive(key, stat_module=stats_module,  epsilon=epsilon, delta=1e-6,
-                                        rounds=rounds, print_progress=True, num_sample=num_sample, debug_fn=debug_fn)
+                                        rounds=rounds, print_progress=True, num_sample=num_sample)
     else:
         sync_data = algo.fit_dp(key, stat_module=stats_module,  epsilon=epsilon, delta=1e-6)
-        debug_fn(1, sync_data)
+
+    sync_data.df.to_csv(path, index=False)
 
     errors = jax.numpy.abs(stats_module.get_true_stats() - stats_module.get_stats_jit(sync_data))
     ave_error = jax.numpy.linalg.norm(errors, ord=1)/errors.shape[0]
@@ -67,38 +65,27 @@ if __name__ == "__main__":
     data = get_data(f'{data_name}-mixed-train',
                     domain_name=f'domain/{data_name}-mixed',  root_path='../../data_files/folktables_datasets_real')
 
-    # algo = PrivGAfast(
-    #     num_generations=100000,
-    #     strategy=SimpleGAforSyncDataFast(
-    #         domain=data.domain,
-    #         data_size=2000,
-    #         population_size=100,
-    #         elite_size=5,
-    #         muta_rate=1,
-    #         mate_rate=1,
-    #     ),
-    #     print_progress=True)
+    algo = PrivGAfast(num_generations=100000,print_progress=True, strategy=SimpleGAforSyncDataFast(
+            domain=data.domain, data_size=2000, population_size=100, elite_size=5, muta_rate=1, mate_rate=1))
 
-    algo = RelaxedProjectionPP(
-        domain=data.domain,
-        data_size=1000,
-        learning_rate=(0.01,),
-        print_progress=False)
+    # algo = RelaxedProjectionPP(domain=data.domain, data_size=1000, learning_rate=(0.01,), print_progress=False)
 
-    train_prefix_module, _ = Halfspace4.get_kway_random_halfspaces(data.domain, k=1, rng=jax.random.PRNGKey(0),random_hs=20000)
-    eval_prefix_module, _ = Halfspace4.get_kway_random_halfspaces(data.domain, k=1, rng=jax.random.PRNGKey(1),random_hs=2000,)
+    train_module, _ = Halfspace4.get_kway_random_halfspaces(data.domain, k=1, rng=jax.random.PRNGKey(0), random_hs=20000)
+    eval_module, _ = Halfspace4.get_kway_random_halfspaces(data.domain, k=1, rng=jax.random.PRNGKey(1), random_hs=2000, )
     # ranges_stat_module, _ = Marginals.get_all_kway_mixed_combinations(data.domain, k_disc=1, k_real=2,
     #                                                                   bins=[2, 4, 8, 16, 32, 64])
 
 
-    # train_prefix_module, _ = Prefix.get_kway_prefixes(data.domain, k=1, rng=jax.random.PRNGKey(0), random_prefixes=1000)
-    # eval_prefix_module, _ = Prefix.get_kway_prefixes(data.domain, k=1, rng=jax.random.PRNGKey(1), random_prefixes=20000)
+    # train_module, _ = Prefix.get_kway_prefixes(data.domain, k=1, rng=jax.random.PRNGKey(0), random_prefixes=1000)
+    # eval_module, _ = Prefix.get_kway_prefixes(data.domain, k=1, rng=jax.random.PRNGKey(1), random_prefixes=20000)
 
+    for eps in [0.07, 0.23, 0.52, 0.74, 1.0]:
+        for r in [10, 25, 50, 75]:
 
-    run_toy_example(algo, data,
-                    stats_module=train_prefix_module,
-                    evaluate_stat_module=eval_prefix_module,
-                    epsilon=1,
-                    rounds=10,
-                    num_sample=30,
-                    adaptive=True)
+            run_acs_example(algo, data,
+                            stats_module=train_module,
+                            evaluate_stat_module=eval_module,
+                            epsilon=eps,
+                            rounds=r,
+                            num_sample=10,
+                            adaptive=True)
