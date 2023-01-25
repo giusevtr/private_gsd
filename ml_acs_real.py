b6ed1693a8c0b341b7222750a6503301cd2b3f44
Jingwu Tang <tangjingwu@pku.edu.cn>
2023-01-25 23:41:47 +0800

    update ml task

--- ml_acs_real.py
+++ ml_acs_real.py
@@ -8,17 +8,18 @@ from sklearn.preprocessing import OneHotEncoder
 
 from utils.utils_data import get_data
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from xgboost.sklearn import XGBClassifier
 
-from sklearn.metrics import accuracy_score, make_scorer,f1_score
+from sklearn.metrics import accuracy_score, make_scorer, f1_score
+
 scorer = make_scorer(accuracy_score)
 # scorer = make_scorer(f1_score, average='weighted')
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def linear_ml_accuracy(df_train, df_test, target='PINCP',model_name='XGBoost'):
+def linear_ml_accuracy(df_train, df_test, target='PINCP', model_name='XGBoost'):
     train_cols = [c for c in df_train.columns if c != target]
     train_cols_num = [c for c in train_cols if domain[c] == 1]
     train_cols_cat = [c for c in train_cols if c not in train_cols_num]
@@ -40,12 +41,18 @@ def linear_ml_accuracy(df_train, df_test, target='PINCP',model_name='XGBoost'):
 
     X_train = np.concatenate((X_train, X_train_cat), axis=1)
     X_test = np.concatenate((X_test, X_test_cat), axis=1)
-    if model_name=='LR':
-        model = LogisticRegression(random_state=0,max_iter=1000)
-    elif model_name=='RF':
+    if model_name == 'LR':
+        model = LogisticRegression(random_state=0, max_iter=1000)
+    elif model_name == 'RF':
         model = RandomForestClassifier(random_state=0)
-    else :
+    elif model_name == 'XGBoost':
         model = XGBClassifier(random_state=0)
+    else:
+        lf = LogisticRegression(random_state=0, max_iter=1000)
+        rf = RandomForestClassifier(random_state=0)
+        xgb = XGBClassifier(random_state=0)
+        estimators = [('lf', lf), ('rf', rf), ('xgb', xgb)]
+        model = VotingClassifier(estimators, voting='hard')
 
     model.fit(X_train, y_train)
 
@@ -61,49 +68,45 @@ def linear_ml_accuracy(df_train, df_test, target='PINCP',model_name='XGBoost'):
     return train_acc, test_acc
 
 
-model_name='LR'
+model_name = 'LR'  # you could modify this for different models
+algo = [
+    'PrivGA',
+    'GEM',
+    'RAP',
+    'RAP++'
+]
+queries = [
+    'Halfspaces',
+    'Prefix',
+    'Ranges'
+]
 ##########
 data_name = f'folktables_2018_real_CA'
 data_train = get_data(f'{data_name}-mixed-train',
-                domain_name=f'domain/{data_name}-mixed', root_path='data_files/folktables_datasets')
+                      domain_name=f'domain/{data_name}-mixed', root_path='data_files/folktables_datasets')
 domain = data_train.domain
 df_train = data_train.df
 
 data_name = f'folktables_2018_real_CA'
 data_test = get_data(f'{data_name}-mixed-test',
-                domain_name=f'domain/{data_name}-mixed', root_path='data_files/folktables_datasets')
+                     domain_name=f'domain/{data_name}-mixed', root_path='data_files/folktables_datasets')
 df_test = data_test.df
-
 
 test_acc_original = {}
 for target in domain.get_categorical_cols():
-    train_acc, test_acc = linear_ml_accuracy(df_train, df_test, target,model_name=model_name)
+    train_acc, test_acc = linear_ml_accuracy(df_train, df_test, target, model_name=model_name)
     test_acc_original[target] = test_acc
     # print(f'target={target}: test_accuracy = {test_acc:.5f}')
-
-sys.exit()
-rounds = 50
 
 sync_paths = []
 acc_list = []
 
 Results = []
 
-algo = [
-    'PrivGA',
-    # 'GEM',
-    # 'RAP',
-    # 'RAP++'
-]
-queries = [
-    'Halfspaces',
-    # 'Prefix',
-    # 'Ranges'
-]
 for a, q in itertools.product(algo, queries):
-    for T in [3,4,5,6,7,8,9]:
+    for T in [3, 4, 5, 6, 7, 8, 9, 10, 20, 40, 60, 80, 25, 50, 75, 100]:
         for eps in [0.07, 0.23, 0.52, 0.74, 1.00]:
-            for seed in [0,1, 2]:
+            for seed in [0, 1, 2]:
                 path = f'examples/acs/results_halfspaces/folktables_2018_real_CA/{a}/{T:03}/{eps:.2f}/sync_data_{seed}.csv'
                 if not os.path.exists(path): continue
                 print(f'reading {path}')
@@ -115,29 +118,19 @@ for a, q in itertools.product(algo, queries):
                     df_train_sync.drop('Unnamed: 0', axis=1, inplace=True)
 
                 cat_cols = domain.get_categorical_cols()
-                print(cat_cols)
                 for cat in domain.get_categorical_cols():
                     df_train_sync[cat] = df_train_sync[cat].round().astype(int)
 
                 for target in cat_cols:
-                    train_acc, test_acc = linear_ml_accuracy(df_train_sync, df_test, target,model_name=model_name)
+                    train_acc, test_acc = linear_ml_accuracy(df_train_sync, df_test, target, model_name=model_name)
                     acc_list.append(test_acc)
 
-                    Results.append([f'{a}({q})', T, eps, seed, f'{target} ML Acc',test_acc_original[target],test_acc])
+                    Results.append([f'{a}({q})', T, eps, seed, f'{target} ML Acc', test_acc_original[target], test_acc])
                     print(path, f'\ntarget ={target}: Original acc = {test_acc_original[target]:.5f},'
                                 f' Synthetic acc = {test_acc:.5f}')
 
-
-
-cols = ['algo', 'T', 'epsilon', 'seed', 'error type','original accuracy','private accuracy']
+cols = ['algo', 'T', 'epsilon', 'seed', 'error type', 'original accuracy', 'private accuracy']
 df_results = pd.DataFrame(Results, columns=cols)
-print(df_results)
-df_results.to_csv(f"ML_result_{model_name}_PrivGA_1000.csv")
-df_results = df_results.groupby(['algo', 'error type', 'T', 'epsilon'], as_index=False)['error'].mean()
-df_results = df_results.groupby(['algo', 'error type', 'epsilon'], as_index=False)['error'].min()
-
-# sns.relplot(data=df_results, x='epsilon', y='error', col='error type', hue='algo', kind='line', markers=True)
-# plt.plot(range(0, len(acc_list)), acc_list, color='b', label='synthetic')
-# plt.hlines(xmin=0, xmax=rounds, y=acc_list[0], color='r', label='original')
-# plt.savefig("ML_result_Xhh.png")
-# plt.show()
+df_results.to_csv(f"ML_{model_name}.csv")
+# df_results = df_results.groupby(['algo', 'error type', 'T', 'epsilon'], as_index=False)['error'].mean()
+# df_results = df_results.groupby(['algo', 'error type', 'epsilon'], as_index=False)['error'].max()
