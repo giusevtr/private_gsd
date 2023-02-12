db1e4b7cc7464c25737e32670d3d82fe9cec3c5a
terranceliu <terranceliu@users.noreply.github.com>
2023-02-12 01:16:08 -0500

    change setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url='https://github.com/giusevtr/private_genetic_algorithm.git',
     author='Giuseppe Vietri, Jingwu Tang, Terrence Liu',
     license='MIT',
-    packages=['examples', 'models', 'stats', 'utils'],
+    packages=['dev', 'models', 'stats', 'utils'],
     zip_safe=False,
     install_requires=['numpy', 'pandas', 'scipy', 'scikit-learn',
                       'tqdm', 'matplotlib', 'seaborn', 'chex',
