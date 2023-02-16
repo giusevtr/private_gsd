54d499fb8008ae3f2d01c0e1f714d9a0632e9af5
Giuseppe Vietri <vietr002@umn.edu>
2023-02-16 14:47:37 -0600

    multitask example

--- dev/examples/acsmulti_example.py
+++ dev/examples/acsmulti_example.py
@@ -6,24 +6,32 @@ from stats import ChainedStatistics, Halfspace, Marginals
 # from utils.utils_data import get_data
 import jax.numpy as jnp
 # from dp_data.data import get_data
-from dp_data import load_domain_config, load_df
+from dp_data import load_domain_config, load_df, get_evaluate_ml
 from utils import timer, Dataset, Domain
 
 if __name__ == "__main__":
     dataset_name = 'folktables_2018_multitask_NY'
-    root_path = '../../dp-data-dev/run/datasets/preprocessed/folktables/1-Year/'
+    root_path = '../../dp-data-dev/datasets/preprocessed/folktables/1-Year/'
     config = load_domain_config(dataset_name, root_path=root_path)
-    df = load_df(dataset_name, root_path=root_path)
+    df_train = load_df(dataset_name, root_path=root_path, idxs_path='seed0/train')
+    df_test = load_df(dataset_name, root_path=root_path, idxs_path='seed0/test')
 
     domain = Domain.fromdict(config)
-    data = Dataset(df, domain)
+    data = Dataset(df_train, domain)
+    targets = ['JWMNP', 'PINCP', 'ESR', 'MIG', 'PUBCOV']
+
     # Get Data
     # data_name = f'folktables_2018_CA'
     # data = get_classification(DATA_SIZE=1000)
 
+    ml_eval_fn = get_evaluate_ml(df_test, config, targets=targets, models=['LogisticRegression'])
+
+    results = ml_eval_fn(df_train , 0)
+    print(results)
+
     # Create statistics and evaluate
+    # TODO: Choose marginals with target labels.
     key = jax.random.PRNGKey(0)
-    # prefix_module = Prefix.get_kway_prefixes(data.domain, k_cat=1, k_num=2, rng=key, random_prefixes=1000)
     prefix_module = Halfspace.get_kway_random_halfspaces(data.domain, k=1, rng=key, random_hs=1000)
     marginal_module2 = Marginals.get_kway_categorical(data.domain, k=2)
     stat_module = ChainedStatistics([
@@ -34,6 +42,13 @@ if __name__ == "__main__":
 
     true_stats = stat_module.get_all_true_statistics()
     stat_fn = stat_module._get_workload_fn()
+
+
+    features = []
+    for f in data.domain.attrs:
+        if f not in targets:
+            features.append(f)
+    train_df = data.df[features]
 
     # algo = RelaxedProjection(domain=data.domain,
     #                         data_size=1000, iterations=1000, learning_rate=0.01,)
@@ -62,6 +77,7 @@ if __name__ == "__main__":
                   f'\t avg error = {errors.mean():.5f}'
                   f'\t time = {timer() - t0:.4f}')
 
+            ml_eval_fn(data.df, 0)
             x = list(range(len(algo.fitness_record)))
             plt.plot(x, algo.fitness_record)
             plt.ylabel('Fitness')
