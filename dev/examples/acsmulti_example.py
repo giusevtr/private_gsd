53425dd4f79325fb5b5e17700f6a7d89612c794a
Giuseppe Vietri <vietr002@umn.edu>
2023-02-19 02:53:52 -0500

    fix chain stats

--- dev/examples/acsmulti_example.py
+++ dev/examples/acsmulti_example.py
@@ -16,9 +16,12 @@ if __name__ == "__main__":
     df_train = load_df(dataset_name, root_path=root_path, idxs_path='seed0/train')
     df_test = load_df(dataset_name, root_path=root_path, idxs_path='seed0/test')
 
+    df_train = df_train.sample(n=1000)
+    df_test = df_test.sample(n=1000)
+
     domain = Domain.fromdict(config)
     data = Dataset(df_train, domain)
-    targets = ['JWMNP', 'PINCP', 'ESR', 'MIG', 'PUBCOV']
+    targets = ['JWMNP_bin', 'PINCP', 'ESR', 'MIG', 'PUBCOV']
 
     # Get Data
     # data_name = f'folktables_2018_CA'
@@ -32,43 +35,35 @@ if __name__ == "__main__":
     # Create statistics and evaluate
     # TODO: Choose marginals with target labels.
     key = jax.random.PRNGKey(0)
-    prefix_module = Halfspace.get_kway_random_halfspaces(data.domain, k=1, rng=key, random_hs=1000)
+    # prefix_module = Halfspace.get_kway_random_halfspaces(data.domain, k=1, rng=key, random_hs=1000)
     marginal_module2 = Marginals.get_kway_categorical(data.domain, k=2)
+    halfspaces = Halfspace(domain=domain, k_cat=1, cat_kway_combinations=[(t, ) for t in targets], rng=key, num_random_halfspaces=100)
     stat_module = ChainedStatistics([
-                                        marginal_module2,
-                                         prefix_module,
+                                            marginal_module2,
+                                            halfspaces,
                                          ])
     stat_module.fit(data)
 
     true_stats = stat_module.get_all_true_statistics()
     stat_fn = stat_module._get_workload_fn()
 
-
-    features = []
-    for f in data.domain.attrs:
-        if f not in targets:
-            features.append(f)
-    train_df = data.df[features]
-
-    # algo = RelaxedProjection(domain=data.domain,
-    #                         data_size=1000, iterations=1000, learning_rate=0.01,)
     # Choose algorithm parameters
     SYNC_DATA_SIZE = 2000
     algo = PrivGA(num_generations=12000,
-                    print_progress=False, stop_early=True,
+                    print_progress=True, stop_early=True,
                     strategy=SimpleGAforSyncData(domain=data.domain, elite_size=5, data_size=SYNC_DATA_SIZE))
 
     delta = 1.0 / len(data) ** 2
     # Generate differentially private synthetic data with ADAPTIVE mechanism
-    for eps in [1.00]:
+    for eps in [100.00]:
     # for eps in [0.07, 0.23, 0.52, 0.74, 1.0]:
         # for seed in [0, 1, 2]:
         for seed in [0]:
             key = jax.random.PRNGKey(seed)
             t0 = timer()
             sync_data = algo.fit_dp_adaptive(key, stat_module=stat_module, epsilon=eps, delta=delta,
-                                             rounds=50,
-                                             num_sample=1
+                                             rounds=10,
+                                             num_sample=3
                                     )
             sync_data.df.to_csv(f'{dataset_name}_sync_{eps:.2f}_{seed}.csv', index=False)
             errors = jnp.abs(true_stats - stat_fn(sync_data.to_numpy()))
