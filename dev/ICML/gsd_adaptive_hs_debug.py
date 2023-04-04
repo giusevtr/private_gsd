9d5a6d1d970e59a056f2dcbc635520830bb2f3a3
Giuseppe Vietri <giusevtr@gmail.com>
2023-04-04 15:48:32 -0400

    update

--- dev/ICML/gsd_adaptive_hs_debug.py
+++ dev/ICML/gsd_adaptive_hs_debug.py
@@ -20,12 +20,16 @@ from dp_data import load_domain_config, load_df, ml_eval
 from dev.dataloading.data_functions.acs import get_acs_all
 
 
-def run(dataset_name,  seeds=(0, 1, 2), eps_values=(0.07, 0.23, 0.52, 0.74, 1.0)):
+def run(dataset_name,
+        seeds=(0, 1, 2),
+        eps_values=(0.07, 0.23, 0.52, 0.74, 1.0),
+        rounds=50,
+        num_sample=5):
     module_name = '2Cat+HS'
     max_num_queries = 200000
-    rounds = 50
-    num_sample = 5
     Res = []
+
+    print(f'Statistics={module_name}, Rounds={rounds}, Samples={num_sample}')
 
     root_path = '../../dp-data-dev/datasets/preprocessed/folktables/1-Year/'
     config = load_domain_config(dataset_name, root_path=root_path)
@@ -37,7 +41,6 @@ def run(dataset_name,  seeds=(0, 1, 2), eps_values=(0.07, 0.23, 0.52, 0.74, 1.0)
     domain = Domain.fromdict(config)
     data = Dataset(df_train, domain)
 
-
     targets = ['JWMNP_bin', 'PINCP', 'ESR', 'MIG', 'PUBCOV']
     # targets = ['PINCP', 'PUBCOV']
     features = []
@@ -46,23 +49,18 @@ def run(dataset_name,  seeds=(0, 1, 2), eps_values=(0.07, 0.23, 0.52, 0.74, 1.0)
             features.append(f)
     ml_fn = ml_eval.get_evaluate_ml(df_test, config, targets, models=['LogisticRegression'])
 
-
-
     # Create statistics and evaluate
     module0 = Marginals.get_kway_categorical(data.domain, k=2)
-
 
     binary_feats = [feat for feat in data.domain.get_categorical_cols() if data.domain.size([feat]) ==2]
     bin_dims = sum(data.domain.project(binary_feats).shape)
     print(f'binary features: ', binary_feats)
     print(f'binary_dim = {bin_dims}')
 
-
     module1 = Halfspace(data.domain, k_cat=1,
                         cat_kway_combinations=[(bf, ) for bf in binary_feats],
                         rng=jax.random.PRNGKey(0),
                         num_random_halfspaces=max_num_queries // bin_dims)
-
 
     stat_module = ChainedStatistics([module0, module1])
     stat_module.fit(data)
@@ -75,8 +73,8 @@ def run(dataset_name,  seeds=(0, 1, 2), eps_values=(0.07, 0.23, 0.52, 0.74, 1.0)
     print(f'Number of queries is {true_stats.shape[0]}.')
 
     algo = PrivGA(num_generations=200000,
-                  strategy=SimpleGAforSyncData(domain, 2000, population_size=100, muta_rate=1, mate_rate=1),
-                  print_progress=False)
+                  strategy=SimpleGAforSyncData(domain, 1000, population_size=100, muta_rate=1, mate_rate=1),
+                  print_progress=True)
 
     delta = 1.0 / len(data) ** 2
     for seed in seeds:
@@ -90,7 +88,7 @@ def run(dataset_name,  seeds=(0, 1, 2), eps_values=(0.07, 0.23, 0.52, 0.74, 1.0)
                                            rounds=rounds,
                                            num_sample=num_sample,
                                            )
-            sync_data.df.to_csv(f'{sync_dir}/sync_data_{seed}.csv', index=False)
+            # sync_data.df.to_csv(f'{sync_dir}/sync_data_{seed}.csv', index=False)
             errors = jnp.abs(true_stats - stat_fn(sync_data))
             elapsed_time = timer() - t0
             print(f'GSD({dataset_name, module_name}): eps={eps:.2f}, seed={seed}'
@@ -99,10 +97,6 @@ def run(dataset_name,  seeds=(0, 1, 2), eps_values=(0.07, 0.23, 0.52, 0.74, 1.0)
                   f'\t time = {elapsed_time:.4f}')
             Res.append(['GSD', dataset_name, module_name, rounds, num_sample, eps, seed, 'Max', errors.max(), elapsed_time])
             Res.append(['GSD', dataset_name, module_name, rounds, num_sample, eps, seed, 'Average', errors.mean(), elapsed_time])
-
-
-
-            # DEbug ML
 
             print(f'ML Debug')
             res = ml_fn(sync_data.df, seed=0)
@@ -115,14 +109,14 @@ def run(dataset_name,  seeds=(0, 1, 2), eps_values=(0.07, 0.23, 0.52, 0.74, 1.0)
                 f1 = row['Score']
                 print(f'target={target:<10}. f1_score={f1:.5f}')
 
-        print()
-
     columns = ['Generator', 'Data', 'Statistics', 'T', 'S', 'epsilon', 'seed', 'error type', 'error', 'time']
     results_df = pd.DataFrame(Res, columns=columns)
     return results_df
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    T = [50]
+    S = [10, 50]
     DATA = [
         'folktables_2018_multitask_CA'
     ]
@@ -134,8 +128,9 @@ if __name__ == "__main__":
         print(f'reading {file_name}')
         results = pd.read_csv(file_name)
     for data in DATA:
-        results_temp = run(data, eps_values=[1.0, 0.74, 0.52, 0.23, 0.07], seeds=[0, 1, 2])
-        results = pd.concat([results, results_temp], ignore_index=True) if results is not None else results_temp
-        print(f'Saving: {file_name}')
+        for rounds, samples in itertools.product(T, S):
+            results_temp = run(data, eps_values=[1.0], seeds=[0], rounds=rounds, num_sample=samples)
+            results = pd.concat([results, results_temp], ignore_index=True) if results is not None else results_temp
+            print(f'Saving: {file_name}')
         # results.to_csv(file_name, index=False)
 
